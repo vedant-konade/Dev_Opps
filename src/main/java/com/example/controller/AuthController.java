332788ed79ecbf483d2e3a417c04c60b2e0959a0
Shravani <132183348+Shrawebwiz@users.noreply.github.com>
2025-02-28 13:10:04 +0530

    to master

---
+++ src/main/java/com/example/controller/AuthController.java
@@ -0,0 +1,22 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+        return "login"; // Maps to login.html
+    }
+
+    @GetMapping("/register")
+    public String showRegistrationPage() {
+        return "register"; // Maps to register.html
+    }
+    @GetMapping("/product-cart")
+    public String showProductPage() {
+        return "product-cart"; // Maps to register.html
+    }
+}
